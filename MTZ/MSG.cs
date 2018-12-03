e46733ea6c065c4cc54b5c3b1e4c909a45b13620
stein_f <felix.stein@rheinmetall.com>
2018-12-03 11:42:39 +0100

    Initial Commit

---
+++ MTZ/MSG.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTZ
+{
+    class MSG
+    {
+        public MSG()
+        {
+
+        }
+
+        public MSG(string messages, string subjects, string sender)
+        {
+            this.sender = sender;
+            this.messages = messages;
+            this.subjects = subjects;
+        }
+
+        public string messages;
+        public string subjects; 
+        public string sender;
+    }
+}
---
+++ MTZ/Stempelzeit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTZ
+{
+    class Stempelzeit
+    {
+        public Stempelzeit(string datum, List<string> zeiten)
+        {
+            this.datum = datum;
+            this.zeiten = zeiten;
+        }
+        public string datum;
+        public List<string> zeiten;
+    }
+}
